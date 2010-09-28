8c673b9923c873ac69bdc946ed21cebf949fe5a5
Martin Billinger <martin.billinger@tugraz.at>
2010-09-28 08:25:36 +0000

    added missing LGPL header to some source files
    git-svn-id: https://libgdf.svn.sourceforge.net/svnroot/libgdf/trunk@17 00a3b082-7228-46c3-a6fa-0e10d54cf57f

--- tools/gdf_merger/main.cpp
+++ tools/gdf_merger/main.cpp
@@ -1,3 +1,20 @@
+//
+// This file is part of libGDF.
+//
+// libGDF is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// libGDF is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with libGDF.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright 2010 Christian Breitwieser
 
 
 //---------------------------------------------------------------------------------------
