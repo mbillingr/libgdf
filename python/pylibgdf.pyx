c274e874d1acff531ccc46ca03f3755ac3bd7651
Martin Billinger <martin.billinger@tugraz.at>
2013-01-10 10:52:19 +0100

    Some more partial python wrapping

--- python/pylibgdf.pyx
+++ python/pylibgdf.pyx
@@ -4,18 +4,106 @@
 from libcpp.vector cimport vector
 from libcpp.string cimport string
 from libcpp cimport bool
+import cython
 
 class WriterFlags:
     writer_ev_file = 0
     writer_ev_memory = 1
     writer_overwrite = 2
+    
+class Datatypes:
+    s2n = {'invalid':0, 'int8':1, 'uint8':2, 'int16':3, 'uint16':4, 'int32':5, 'uint32':6, 'int64':7, 'uint64':8, 'float32':16, 'float64':17}
+    n2s = dict((v,k) for k,v in Datatypes.s2n.iteritems())
+    
+string_encoding = 'ascii'
+
+ctypedef fused numeric:
+    cython.int
+    cython.uint
+    
+# =================================================================================
+    
+# GDF::SignalHeader - C++ Interface
+cdef extern from "GDF/SignalHeader.h" namespace "gdf":
+    cdef cppclass SignalHeader:
+        SignalHeader( )        
+        void set_label( string )
+        string get_label( )
+        void set_transducer_type( string )
+        string get_transducer_type( )
+        void set_physical_dimension( string )
+        string get_physical_dimension( )
+        void set_physical_dimension_code( unsigned short )
+        unsigned short get_physical_dimension_code( )
+        void set_physmin( double )
+        double get_physmin( )
+        void set_physmax( double )
+        double get_physmax( )
+        void set_digmin( double )
+        double get_digmin( )
+        void set_digmax( double )
+        double get_digmax( )
+        void set_lowpass( float )
+        float get_lowpass( )
+        void set_highpass( float )
+        float get_highpass( )        
+        void set_notch( float )
+        float get_notch( )
+        void set_datatype( unsigned int )
+        unsigned int get_datatype( )
+        void set_sensor_pos( float )
+        float get_sensor_pos( )
+        void set_sensor_info( float )
+        float get_sensor_info( )
+        void set_samplerate( float )
+        float get_samplerate( )
+        
+# =================================================================================
+
+# GDF::GDFHeaderAccess - C++ Interface
+cdef extern from "GDF/GDFHeaderAccess.h" namespace "gdf":
+    ctypedef GDFHeaderAccess* const_GDFHeaderAccess_ptr "const gdf::GDFHeaderAccess*"
+    cdef cppclass GDFHeaderAccess:
+        size_t getNumSignals()
+        SignalHeader& getSignalHeader( size_t )
+        SignalHeader& getSignalHeader_readonly( size_t )
+        SignalHeader& getMainHeader( size_t )
+        SignalHeader& getMainHeader_readonly( size_t )
+        vector[unsigned short] getSignalIndices( )
+
+# HeaderWrapper - Python Class
+cdef class HeaderWrapper:
+    '''class that provides access to GDF header information'''
+    cdef GDFHeaderAccess *header_w  # for writing
+    cdef const_GDFHeaderAccess_ptr header_r  # for reading
+    def get_num_signals(self):
+        return self.header_r.getNumSignals()
+               
+    def set_label(self, idx, label):
+        self.header_w.getSignalHeader(idx).set_label( label.encode(string_encoding) )
+    def get_label(self, idx):
+        return self.header_r.getSignalHeader_readonly(idx).get_label( ).strip().decode(string_encoding)
+    def get_labels(self):
+        indices = self.header_r.getSignalIndices()
+        return [self.get_label(i) for i in indices]
+        
+    def set_datatype(self, idx, type):
+        self.header_w.getSignalHeader(idx).set_datatype( Datatypes.s2n[type] )
+    def get_datatype(self, idx):
+        return Datatypes.n2s[self.header_r.getSignalHeader_readonly(idx).get_datatype()]
+    def get_datatypes(self):
+        indices = self.header_r.getSignalIndices()
+        return [self.get_datatype(i) for i in indices]
+        
+
+# =================================================================================
 
 # GDF::Writer - C++ interface
 cdef extern from "GDF/Writer.h" namespace "gdf":
     cdef cppclass Writer:
         Writer( ) except +
         void open( int ) except +
-        void open( string, int ) except +
+        #void open( string, int ) except +
         void close( ) except +
         bool isOpen( )
         void setFilename( string ) except +
@@ -42,19 +130,21 @@ cdef extern from "GDF/Writer.h" namespace "gdf":
         #void addEvent( const Mode3Event &ev );
         void addEvent( unsigned int, unsigned short, unsigned short, unsigned int )
         #void addEvent( uint32 position, uint16 type, uint16 channel, float32 value );
-        #const GDFHeaderAccess &getHeaderAccess_readonly( ) const 
-        #GDFHeaderAccess &getHeaderAccess( ) 
+        GDFHeaderAccess &getHeaderAccess_readonly( ) 
+        GDFHeaderAccess &getHeaderAccess( ) 
         #const MainHeader &getMainHeader_readonly( ) const
         #MainHeader &getMainHeader( )
         #const SignalHeader &getSignalHeader_readonly( size_t idx ) const 
         #inline size_t getNumSignals( ) const
-        #SignalHeader &getSignalHeader( size_t idx )
+        #SignalHeader &getSignalHeader( size_t )
         
 # GDF::Writer - Python wrapper
-cdef class GDFWriter:
+cdef class GDFWriter(HeaderWrapper):
     cdef Writer *thisptr
     def __cinit__( self ):        
         self.thisptr = new Writer( )
+        self.header_w = &self.thisptr.getHeaderAccess()
+        self.header_r = &self.thisptr.getHeaderAccess_readonly()
     def __dealloc__( self ):
         del self.thisptr
     def open( self, flags=WriterFlags.writer_ev_file ):
@@ -62,10 +152,10 @@ cdef class GDFWriter:
     def isOpen(self):
         return self.thisptr.isOpen( )
     def setFilename(self, filename):
-        self.thisptr.setFilename( filename )
+        self.thisptr.setFilename( filename.encode(string_encoding) )
     def setMaxFullRecords(self, number):
         self.thisptr.setMaxFullRecords( number )
-    def createSignal(self, index, throwexc):
+    def createSignal(self, index, throwexc = True):
         self.thisptr.createSignal( index, throwexc )
     def getFirstFreeSignalIndex(self):    
         return self.thisptr.getFirstFreeSignalIndex( )
@@ -84,7 +174,7 @@ cdef class GDFWriter:
     def addEvent3( self, position, type, channel, duration ):
         self.thisptr.addEvent( position, type, channel, duration )
     
-    
+# =================================================================================
     
     
     #EOF
